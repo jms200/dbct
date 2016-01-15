c1444bc1887ed8a3dae043c8a6be05deae964815
jmstanley84 <jstanley84@me.com>
2016-01-15 14:32:42 -0500

    Changes to be committed: 	modified:   dbct.rb 	Added some required option checks to the redis db type and made sure a key was inserted, retrieve, and deleted to confirm connectivity.

--- dbct.rb
+++ dbct.rb
@@ -9,7 +9,7 @@ packages.each { |x| require x }
 options = OpenStruct.new
 OptionParser.new do |opt|
   opt.on('--db-type <mongo|rethink>', 'Type of database to connect to'){ |o| options.dbtype = o }
-  opt.on('--db DATABASE', 'Database to connect to'){ |o| options.db = o.to_i }
+  opt.on('--db DATABASE', 'Database to connect to'){ |o| options.db = o }
   opt.on('-m', '--mongo-conn MONGO-CONNECTION', 'Connection string required for MongoDB in form <host:port>,<host:port>/<db>?replicaSet=<set_id>'){ |o| options.connection = o }
   opt.on('--host HOST', 'Host to connect to'){ |o| options.host = o }
   opt.on('--port PORT', 'Connection port'){ |o| options.port = o.to_i }
@@ -42,16 +42,51 @@ when 'rethink'
   dbs=r.db_list().run(conn)
   print(dbs)
 when 'redis'
-  redis = Redis.new(:host => options.host,
-                    :port => options.port,
-                    :db => options.db,
-                    :password => options.password
-  )
 
-redis.set("connected", "connection was established")
+  #flag used to determine if we can complete the connection to the redis database.
+  redisError = false
 
-puts redis.get("connected")
+  #check if required parameters are set
+  if options.host.eql?(nil)
+    puts 'Error: Redis host not set. Use --host <HOST> to set'
+    redisError = true
+  elsif options.port.nil?
+    puts 'Error: Redis port not set. Use --port <PORT> to set'
+    redisError = true
+  elsif options.db.eql?(nil)
+    puts 'Error: Redis database not set. Use --db <DATABSE> to set'
+    redisError = true
+  elsif options.password.eql?(nil)
+    puts 'Error: Redis password not set. Use --pass <PASSWORD> to set'
+    redisError = true
+  else #Clears configuration and prints a status message
+    puts 'Configuring redis connection'
+  end
 
-else
+  #if no errors then we test the connection.
+  if redisError != true
+
+    #connection configuration
+    redis = Redis.new(:host => options.host,
+                      :port => options.port,
+                      :db => options.db,
+                      :password => options.password
+    )
+
+    #Inserts a key into the redis deployment
+    puts 'Inserting key into redis "connected" => ""connection was established\"'
+    redis.set("connected", "connection was established")
+    puts 'Inserted successfully'
+
+    #Gets the key from the redis deployment to confirm connectivity
+    puts 'Getting \"connected\" key from redis deployment'
+    puts redis.get("connected")
+
+    #Removes the key from the redis deployment
+    puts 'Deleting \"connected\" key from redis deployment'
+    redis.del("connected")
+    puts 'Deleted successfully'
+  end
+else #Prints an error if the user doesn't specify a db-type
   puts 'Error: No database type selected use the \"--db-type\" option. Run \"MongoConnectionTester.rb -h\" for more details'
 end #case options.dbtype
